0d1dc1ff1f88c6cc554c7b8eaa984fb534c99de7
pavel_erebakan <ppvr3407@gmail.com>
2021-02-20 21:04:06 +0200

    создал проект хедера с елементом слайдера

---
+++ script/script.js
@@ -0,0 +1,13 @@
+let index = document.querySelector(".header");
+
+function format_1() {
+    index.style.left = 0 + 'px';
+}
+
+function format_2() {
+    index.style.left = -1366 + 'px';
+}
+
+function format_3() {
+    index.style.left = -2732 + 'px';
+}
